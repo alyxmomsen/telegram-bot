db91c14380ec15cbd860287670e4978a9cc40daa
root <root@2627997-fv81320.twc1.net>
2024-03-14 14:13:36 +0300

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-// const MyBotShell = require("./my-bot-shell/my-bot-shell");
 
 const { MyTelegrafFramework } = require("./my-bot-shell/my-bot-shell");
-// const { current_token } = require("../src/bottoken");
-// const uri = "mongodb://localhost:27017";
 
 const bot = new MyTelegrafFramework();
 
 bot.launch();
 
-// const bot = new TelegrafFramework(current_token, uri);
 
-// bot.addEventHandler("hears", (ctx) => {});
-
-// bot.launch();
